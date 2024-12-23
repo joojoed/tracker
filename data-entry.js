2485bcbda0475eda435d871a3eaf2945834f9b3c
joojoed <delwlied@gmail.com>
2024-12-23 10:37:26 +0400

    Update data-entry.js

--- data-entry.js
+++ data-entry.js
@@ -5,7 +5,7 @@
        const email = document.getElementById('email').value;
        const message = document.getElementById('message').value;
 
-       fetch('https://script.google.com/macros/s/AKfycbyVfHRXIxAACSu-cp7RcElG4E-_vRo4JAlKqO0VfMEANK-k5f-MIkEd0xEvCp2xJ9gq/exec', {
+       fetch('https://script.google.com/macros/s/AKfycbxgG-O2niXBFgNDFIwrnOpbE0iLSk9ixW0b4lKJoZ_Osj1pAJPgHvdAmrk-lrhCdZdy/exec', {
            method: 'POST',
            headers: {
                'Content-Type': 'application/json',
