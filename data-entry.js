19610dfe0495f292d78860cc5d49a8c64520a011
joojoed <delwlied@gmail.com>
2024-12-23 09:00:52 +0400

    Update data-entry.js

--- data-entry.js
+++ data-entry.js
@@ -7,7 +7,7 @@ document.getElementById('data-entry-form').addEventListener('submit', function(e
 
     // Example method to send data to Google Sheets
     // You need to replace with the specific API call and credentials
-    fetch('https://script.google.com/macros/s/AKfycby2Wvn2BBCaqHFYCtWH91j6nGyl9E5hAYTPSyfAnEE5JtTitRAkMURDf6Z8x5CuTRbR/exec', {
+    fetch('https://script.google.com/macros/s/AKfycbyobuL4TqzTUKlk9YT65LFTJTqhqJ88vJdb7ulQtOUb2sBGb6EAC9pJ0haC_mAJCPZi/exec', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
